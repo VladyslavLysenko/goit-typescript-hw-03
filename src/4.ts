9fc4d5914ff307209d0bcb5dfb88b421b4397d69
Nancy311 <105671987+VladyslavLysenko@users.noreply.github.com>
2024-06-25 15:27:51 +0300

    finally

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.floor(Math.random() * 1000000);
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    } else {
+      console.log("Door is closed. Can't enter.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key === this.key) {
+      this.door = true;
+      console.log('Door is open.');
+    } else {
+      console.log('Wrong key. Door is still closed.');
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +60,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
